83c0b7c28d43f3df88aaaa73232f5328561117be
becker63 <johnsontaylor6320@gmail.com>
2026-05-02 14:28:42 -0400

    new blog

--- lib/mdx.ts
+++ lib/mdx.ts
@@ -1,12 +1,18 @@
 import rehypeHighlight from "rehype-highlight";
 import rehypeRaw from "rehype-raw";
 import { all } from "lowlight";
+import pkl from "@apple/highlightjs-pkl";
 import remarkMermaidStatic from "./remark-mermaid-static";
 import type { MDXRemoteProps } from "next-mdx-remote/rsc";
+
+const languages = {
+  ...all,
+  pkl,
+};
 
 export const mdxOptions: MDXRemoteProps["options"] = {
   mdxOptions: {
     remarkPlugins: [remarkMermaidStatic],
-    rehypePlugins: [rehypeRaw, [rehypeHighlight, { languages: all }]] as any,
+    rehypePlugins: [rehypeRaw, [rehypeHighlight, { languages }]] as any,
   },
 };
